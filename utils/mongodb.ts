d391870656821c69e5bd2162092b17a9bde79a05
Calebbuffleben <calebbuffleben@gmail.com>
2023-09-09 18:17:10 -0300

    Connect with mongodb

---
+++ utils/mongodb.ts
@@ -0,0 +1,12 @@
+import { MongoClient } from "mongodb";
+
+const uri = 'mongodb://127.0.0.1:27017/';
+
+export async function connectToDatabase() {
+    const client = await MongoClient.connect(uri);
+  
+    const db = client.db();
+  
+  
+    return { client, db };
+  }
