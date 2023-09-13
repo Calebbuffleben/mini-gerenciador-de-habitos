1f066ceca152f6efb2d3fd2d35af81f44cdba46e
Calebbuffleben <calebbuffleben@gmail.com>
2023-09-13 15:00:53 -0300

    decoded habit

--- app/habito/[habit]/page.tsx
+++ app/habito/[habit]/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Habit = ({ params: {habit} }: { params: { habit: string } }) => {
+    const decodedHabit = decodeURIComponent(habit);
 
+    return (
+        <h1>{decodedHabit}</h1>
+    )
 }
 
 export default Habit;
