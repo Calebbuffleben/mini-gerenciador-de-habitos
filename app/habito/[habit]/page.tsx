b5fe769642bffbbad5ddc7272a113b178e73faad
Calebbuffleben <calebbuffleben@gmail.com>
2023-09-23 07:19:15 -0300

    style habit page

--- app/habito/[habit]/page.tsx
+++ app/habito/[habit]/page.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { connectToDatabase } from "../../../utils/mongodb"
 
 const Habit = async ({ params: {habit} }: { params: { habit: string } }) => {
-    const decodedHabit = decodeURIComponent(habit);
+    const decodedHabit = decodeURI(habit);
     const { db } = await connectToDatabase();
 
     return (
-        <h1>{decodedHabit}</h1>
+        <main className="container relative flex flex-col gap-8 px-12 pt-16">
+            <h1 className="text-4xl font-light text-center text-white font-display">
+                {decodedHabit}
+            </h1>
+        </main>
     )
 }
 
