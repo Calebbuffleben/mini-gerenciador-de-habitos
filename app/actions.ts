f6f6756604313bdcf9ab9d4cd6f208d694fd157f
Calebbuffleben <calebbuffleben@gmail.com>
2023-10-11 17:47:54 -0300

    actions

--- app/actions.ts
+++ app/actions.ts
@@ -1,5 +1,42 @@
-"use server"
+"use server";
 
-export async function deleteHabit() {
-    
+import { kv } from "@vercel/kv";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+type toogleHabit = {
+  habit: string;
+  habitStreak: Record<string, boolean> | null;
+  date: string | null;
+  done?: boolean
+}
+
+export async function deleteHabit(habit: string) {
+  await kv.hdel("habits", habit);
+
+  revalidatePath("/")
+}
+
+export async function newHabit(formData: FormData) {
+  "use server";
+
+  const habit = formData.get("habit");
+  await kv.hset("habits", {[habit as string]: {}})
+
+  revalidatePath('/')
+  redirect('/')
+}
+
+export async function toggleHabit({habit, habitStreak, date, done}: toogleHabit) {
+  if (!habitStreak || !date) return
+
+  const updatedHabitStreak = {
+    [habit]: {
+      ...habitStreak,
+      [date]: done === undefined ? true : !done
+    }
+  }
+
+  await kv.hset("habits", updatedHabitStreak)
+  revalidatePath("/")
 }
