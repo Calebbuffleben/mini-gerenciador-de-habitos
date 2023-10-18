f1f48db315cb64f26edb9cc0e728dd011f4904fe
Calebbuffleben <calebbuffleben@gmail.com>
2023-10-17 23:22:06 -0300

    habit update

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${dosis.variable} ${inter.variable} flex items-center flex-col mt-10 bg-neutral-900`}>
         <Image src="/images/logo.svg" width={200} height={200} />
+        
         {children}
       </body>
     </html>
