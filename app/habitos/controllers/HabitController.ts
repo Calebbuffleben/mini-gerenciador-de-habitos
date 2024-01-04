edf5d8540a72f04cf51ad53cb773ca8ea8457c30
Calebbuffleben <calebbuffleben@gmail.com>
2024-01-03 22:20:17 -0300

    habits

--- app/habitos/controllers/HabitController.ts
+++ app/habitos/controllers/HabitController.ts
@@ -1,4 +1,5 @@
 import IHabit from "@/app/interfaces/IHabit";
+import { connectToDatabase } from "@/utils/mongodb";
 import HabitService from "../services/HabitService";
 
 class HabitController {
@@ -7,7 +8,14 @@ class HabitController {
         const habitService = new HabitService();
         const response = await habitService.createHabit(habit);
 
-        return { message: 'Habit Added', habit };
+        return { message: 'Habit Added', response };
+    }
+
+    async getHabits() {
+        const { db } = await connectToDatabase();
+        const habits = await db.collection('habits').find({}).toArray();
+
+        return { habits };
     }
 }
 
