9f7f0b4670f1fa0682ab412a118b388ce9090f10
Calebbuffleben <calebbuffleben@gmail.com>
2024-01-08 11:26:25 -0300

    change folder

--- app/habitos/(pages)/habito/[habit]/page.tsx
+++ app/habitos/(pages)/habito/[habit]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import CalendarComponent from "../../../../components/Calendar/CalendarComponent";
-import HabitController from "../../controllers/HabitController";
+import CalendarComponent from "../../../../../components/Calendar/CalendarComponent";
+import HabitController from "../../../controllers/HabitController";
 
 const Habit = async ({ params: {habit} }: { params: { habit: string } }) => {
     const decodedHabit = decodeURI(habit);
